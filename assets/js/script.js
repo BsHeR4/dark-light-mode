6823d0d69aa87cd8e484e676f306b4f4c5291be5
BsHeR4 <bshermahayni@gmail.com>
2025-06-30 15:04:12 +0300

    fix bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
-
 function darkMode() {
-    let body = document.querySelector('body');
+    let hero = document.querySelector('.hero');
+    let nav = document.querySelector('nav');
     let darkModeButton = document.querySelector('.fa-moon');
     let lightModeButton = document.querySelector('.fa-sun');
 
-    body.classList.toggle('darkMode');
+    hero.classList.toggle('darkMode');
+    nav.classList.toggle('darkMode');
     darkModeButton.classList.toggle('active');
     lightModeButton.classList.toggle('active');
 }
